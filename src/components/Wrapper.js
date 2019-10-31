8b42fdee40379a64fc466aade818a071ff1e0331
mohamed_abu_galala <mohamed.abugalala@gmail.com>
2019-10-31 17:10:03 +0200

    Prepare example

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useRef } from "react";
+// , useState
 import ChildItem from "./ChildItem";
 
 let timeOutHandles = [];
@@ -9,10 +10,29 @@ const updateTimeOut = (idx, value) => {
 };
 
 const Wrapper = props => {
+  // const [validChildren, setValidChildren] = useState([]);
   const isInitialMount = useRef(true);
 
   useEffect(() => {
     if (isInitialMount.current) {
+      //
+      //
+      //
+      // get tree dfs
+      const getValidChildren = cur => {
+        const childs = cur.props.children;
+        if (typeof childs === "object")
+          for (let i = 0; i < childs.length; i++) {
+            const child = childs[i];
+            if (typeof child === "object") getValidChildren(child);
+            else if (child.toString().trim().length) console.log(child);
+          }
+        else if (childs.toString().trim().length) console.log(childs);
+      };
+
+      getValidChildren({ props: { children: props.children } });
+
+      console.log(props.children);
       isInitialMount.current = false;
       timeOutHandles = Array(props.children.length).fill();
     }
