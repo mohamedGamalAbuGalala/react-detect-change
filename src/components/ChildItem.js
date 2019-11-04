8832ecedce92a4c8a500885028ae92b42618a9c6
mohamed_abu_galala <mohamed.abugalala@gmail.com>
2019-11-04 14:31:21 +0200

    Find element which have text and ignore others

--- src/components/ChildItem.js
+++ src/components/ChildItem.js
@@ -2,25 +2,29 @@ import React, { useRef, useEffect } from 'react';
 import { useHighlight } from '../hooks/useHighlight';
 
 const ChildItem = props => {
-	const isInitialMount = useRef(true);
+  const isInitialMount = useRef(true);
 
-	useEffect(() => {
-		if (isInitialMount.current) {
-			isInitialMount.current = false;
-		}
-	}, [props]);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    }
+  }, [props]);
 
-	const child = props.child;
-	useHighlight(
-		props.updateTimeOut,
-		props.idx,
-		props.timeOut,
-		child.ref,
-		props.timeOutHandles,
-		isInitialMount.current,
-		[Array.isArray(child.props.children) ? child.props.children[0] : child.props.children]
-	);
-	return <></>;
+  const child = props.child;
+  useHighlight(
+    props.updateTimeOut,
+    props.idx,
+    props.timeOut,
+    child.ref,
+    props.timeOutHandles,
+    isInitialMount.current,
+    [
+      Array.isArray(child.props.children)
+        ? child.props.children.find(c => c.toString().trim().length)
+        : child.props.children
+    ]
+  );
+  return <></>;
 };
 
 export default ChildItem;
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -5,82 +5,82 @@ import ChildItem from './ChildItem';
 let timeOutHandles = [];
 
 const updateTimeOut = (idx, value) => {
-	timeOutHandles[idx] = value;
+  timeOutHandles[idx] = value;
 };
 
 const Wrapper = props => {
-	const [validChildren, setValidChildren] = useState([]);
-	const isInitialMount = useRef(true);
+  const [validChildren, setValidChildren] = useState([]);
+  const isInitialMount = useRef(true);
 
-	//
-	//
-	//
-	// get tree dfs
-	const getValidChildren = useCallback(cur => {
-		const children = cur.props.children;
-		if (Array.isArray(children))
-			for (let i = 0; i < children.length; i++) {
-				const child = children[i];
-				if (typeof child === 'object') getValidChildren(child);
-				else if (child.toString().trim().length) {
-					// eslint-disable-next-line no-loop-func
-					setValidChildren(_c => {
-						if (_c) return [..._c, cur];
-						return [cur];
-					});
-					// console.log(cur, child);
-				}
-			}
-		else if (typeof children === 'object') {
-			cur = children;
-			setValidChildren(_c => {
-				if (_c) return [..._c, cur];
-				return [cur];
-			});
-			// console.log('+++', cur, Array.isArray(cur.props.children) ? cur.props.children[0] : cur.props.children);
-		} else if (children.toString().trim().length) {
-			setValidChildren(_c => {
-				if (_c) return [..._c, cur];
-				return [cur];
-			});
-			// console.log('***', cur, children);
-		}
-	}, []);
+  //
+  //
+  //
+  // get tree dfs
+  const getValidChildren = useCallback(cur => {
+    const children = cur.props.children;
+    if (Array.isArray(children))
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        if (typeof child === 'object') getValidChildren(child);
+        else if (child.toString().trim().length) {
+          // eslint-disable-next-line no-loop-func
+          setValidChildren(_c => {
+            if (_c) return [..._c, cur];
+            return [cur];
+          });
+          // console.log(cur, child);
+        }
+      }
+    else if (typeof children === 'object') {
+      cur = children;
+      setValidChildren(_c => {
+        if (_c) return [..._c, cur];
+        return [cur];
+      });
+      // console.log('+++', cur, Array.isArray(cur.props.children) ? cur.props.children[0] : cur.props.children);
+    } else if (children.toString().trim().length) {
+      setValidChildren(_c => {
+        if (_c) return [..._c, cur];
+        return [cur];
+      });
+      // console.log('***', cur, children);
+    }
+  }, []);
 
-	useEffect(() => {
-		if (isInitialMount.current) {
-			isInitialMount.current = false;
-		}
-		setValidChildren(_c => []);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    }
+    setValidChildren(_c => []);
 
-		getValidChildren({ props: { children: props.children } });
+    getValidChildren({ props: { children: props.children } });
 
-		setValidChildren(_c => _c.filter(c => c));
+    setValidChildren(_c => _c.filter(c => c));
 
-		// console.log(props.children);
-	}, [getValidChildren, props.children]);
+    // console.log(props.children);
+  }, [getValidChildren, props.children]);
 
-	useEffect(() => {
-		timeOutHandles = Array(validChildren.length).fill();
-		// console.log(validChildren);
-	}, [validChildren.length]);
+  useEffect(() => {
+    timeOutHandles = Array(validChildren.length).fill();
+    // console.log(validChildren);
+  }, [validChildren.length]);
 
-	return (
-		<>
-			{validChildren.map((child, idx) => (
-				<ChildItem
-					key={idx}
-					{...props}
-					idx={idx}
-					updateTimeOut={updateTimeOut}
-					timeOutHandles={timeOutHandles[idx]}
-					child={child}
-				></ChildItem>
-			))}
+  return (
+    <>
+      {validChildren.map((child, idx) => (
+        <ChildItem
+          key={idx}
+          {...props}
+          idx={idx}
+          updateTimeOut={updateTimeOut}
+          timeOutHandles={timeOutHandles[idx]}
+          child={child}
+        ></ChildItem>
+      ))}
 
-			{props.children}
-		</>
-	);
+      {props.children}
+    </>
+  );
 };
 
 export default Wrapper;
